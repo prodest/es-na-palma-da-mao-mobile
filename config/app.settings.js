728bc09b16ca4f138dab5e0dc5746a7bf969e281
hoisel <danielhoiselferraz@gmail.com>
2017-01-31 14:43:32 -0200

    feat(geral): ajusta settings para usar ambiente developer por padrão
    ajusta settings para usar ambiente developer por padrão( Configurações relativas ao acesso cidadão
    
    ).

--- config/app.settings.js
+++ config/app.settings.js
@@ -12,7 +12,7 @@ const shared = {
     push: 'https://api.es.gov.br/push',
     espm: 'https://api.es.gov.br/espm',
     transparency: 'https://api.es.gov.br/transparency',
-    acessocidadao: process.env.API_ACESSO_CIDADAO_URL || 'https://sistemas.es.gov.br/prodest/acessocidadao.developer.webapi/api'
+    acessocidadao: process.env.API_ACESSO_CIDADAO_URL || 'https://developers.es.gov.br/acessocidadao.webapi/'
   },
   push: {
     senderId: process.env.PUSH_SENDER_ID,
@@ -31,19 +31,19 @@ const shared = {
   },
   locale: 'pt-br',
   identityServer: {
-    url: process.env.IDENTITY_SERVER_URL || 'https://sistemas.es.gov.br/prodest/acessocidadao.developer/is',
+    url: process.env.IDENTITY_SERVER_URL || 'https://developers.es.gov.br/acessocidadao/is',
     scopes: [ 'cpf', 'nome', 'email', 'dataNascimento', 'filiacao', 'celular', 'telefone', 'documentos', 'ApiAcessoCidadao', 'openid', 'offline_access' ],
     defaultScopes: 'openid offline_access ApiAcessoCidadao cpf nome email documentos',
     publicKey: '-----BEGIN PUBLIC KEY-----\nMIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEArYaYlPnnrxBVwC4o0ykG\nVg8gjH/TerrrXS3GmsZeON6SCNuOBzUj+7RiEF64lE//gLY01nTJZtnUIPvmKJW/\n1+eWxGNW1Mh1JpT/f3A6Q5rp2WNKSBwvEFPE58lkD63Tewsn3+0dw+aFKaSW+l3A\nZ7WS4AxXxBLIRr2zpTL3DOCbeT/m2yEQ8Do662/d+ty7F08FJVaaz2PxmnLEeSQX\n6RTRPeFRPlGVj91H4h85Ln+0Oc0U/oiqa+AKwobWXLOqDKhn8HYZuoya368TqZ9X\n26QEp1g7psaT8kiNRFAt0Yb4WbgFSWf2r92HDS8dj25TNTeeLkvZ48KylTKU23DT\nqQIDAQAB\n-----END PUBLIC KEY-----',
     clients: {
       espm: {
         id: process.env.IDENTITY_SERVER_ESPM_ID || '0f06212c-aac4-482e-9489-c50789ceaa5c',
-        secret: process.env.IDENTITY_SERVER_ESPM_SECRET || '39acabf2-2561-4e69-b9cf-89d77fd9f111'
+        secret: process.env.IDENTITY_SERVER_ESPM_SECRET || '123123123'
       },
 
       espmExternalLoginAndroid: {
         id: process.env.IDENTITY_SERVER_EXTERNAL_LOGIN_ID || '0184531e-ca21-463b-ba67-2642997af511',
-        secret: process.env.IDENTITY_SERVER_EXTERNAL_LOGIN_SECRET || '5e801b38-8ba2-4461-8faf-dd19b49e8f73'
+        secret: process.env.IDENTITY_SERVER_EXTERNAL_LOGIN_SECRET || '123123123'
       }
     }
   },
